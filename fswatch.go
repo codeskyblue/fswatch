2f4cf54562ae3c03006b78c149d295d4acfe86ed
jeromenerf <jerome.andrieux@gmail.com>
2016-04-01 01:31:03 +0200

    Pass config file as a flag, using YAML as fallback

--- fswatch.go
+++ fswatch.go
@@ -472,6 +472,7 @@ func InitFWConfig() {
 
 func main() {
 	version := flag.Bool("version", false, "Show version")
+	configfile := flag.String("config", FWCONFIG_YAML, "Show version")
 	flag.Parse()
 
 	if *version {
@@ -483,7 +484,7 @@ func main() {
 	var fwc FWConfig
 	var err error
 	if subCmd == "" {
-		fwc, err = readFWConfig(FWCONFIG_JSON, FWCONFIG_YAML)
+		fwc, err = readFWConfig(*configfile, FWCONFIG_JSON)
 		if err == nil {
 			subCmd = "start"
 		} else {
